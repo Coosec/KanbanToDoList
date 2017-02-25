d794b27c0b2931dcb63b0ac1b2fce9c9f07a2a65
Konrad Zapala <zapala.konrad@gmail.com>
2017-02-25 11:47:38 +0100

    Display kanban task in 3 states: open, wip, closed. Added some actions for change task state.

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,14 +1,13 @@
 class TasksController < ApplicationController
   before_action :authenticate_user!
-  before_action :set_task, only: [:show, :edit, :update, :destroy]
+  before_action :set_task, only: [:show, :edit, :update, :destroy, :change]
 
   # GET /tasks
   # GET /tasks.json
   def index
-    #@open = current_user.tasks.where(state: "open")
-    #@wip = current_user.tasks.where(state: "wip")
-    #@closed = current_user.tasks.where(state: "closed")
-    @tasks = current_user.tasks
+    @open = current_user.tasks.where(state: "open")
+    @wip = current_user.tasks.where(state: "wip")
+    @closed = current_user.tasks.where(state: "closed")
   end
 
   # GET /tasks/1
@@ -62,6 +61,13 @@ class TasksController < ApplicationController
     respond_to do |format|
       format.html { redirect_to tasks_url, notice: 'Task was successfully destroyed.' }
       format.json { head :no_content }
+    end
+  end
+  
+  def change
+    @task.update_attributes(state: params[:state])
+      respond_to do |format| 
+      format.html { redirect_to tasks_path, notice: "Task updated"}
     end
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,10 @@ Rails.application.routes.draw do
   devise_for :users
   get 'pages/home'
   root 'pages#home'
-
+  resources :tasks do
+      member do
+          put :change
+      end
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
