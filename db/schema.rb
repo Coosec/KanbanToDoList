ec902769a4a68268683603ce8119611bb1a07595
Konrad Zapala <zapala.konrad@gmail.com>
2017-02-25 11:19:02 +0100

    Connect tasks with user, added migration, small html changes.

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170225094203) do
+ActiveRecord::Schema.define(version: 20170225100104) do
 
   create_table "tasks", force: :cascade do |t|
     t.text     "content"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",                  null: false
+    t.datetime "updated_at",                  null: false
+    t.string   "state",      default: "open"
+    t.integer  "user_id"
   end
 
   create_table "users", force: :cascade do |t|
